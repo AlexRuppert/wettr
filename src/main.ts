8ddd329b450fea4d2350d27ccb4a0846f2cbed1b
Alexander Ruppert <alexander.ruppert@rwth-aachen.de>
2021-07-27 22:07:10 +0200

    pwa

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import App from './App.svelte'
 
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onOfflineReady() {
+    // show a ready to work offline to user
+  },
+})
 const app = new App({
-  target: document.getElementById('app')
+  target: document.getElementById('app'),
 })
 
 export default app
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,38 @@
 import { defineConfig } from 'vite'
 import svelte from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  base: ''
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Wettr',
+        short_name: 'Wettr',
+        icons: [
+          {
+            src: '/favicon/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/favicon/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        lang: 'de',
+        scope: './',
+        start_url: './',
+      },
+    }),
+    svelte(),
+  ],
+  base: '',
 })
