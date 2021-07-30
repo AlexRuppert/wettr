bdfd00f53e8c4bc0d4aeffe303887b96c9b47d38
Alexander Ruppert <alexander.ruppert@rwth-aachen.de>
2021-07-31 00:43:36 +0200

    parallelized api request

--- src/logic/reloader.ts
+++ src/logic/reloader.ts
@@ -8,12 +8,18 @@ let nextCheckTime = 0
 
 export async function reload() {
   try {
-    currentWeatherData.set(
-      await Weather.getCurrentWeather(coordinates.lat, coordinates.lon)
+    const currentWeatherRequest = Weather.getCurrentWeather(
+      coordinates.lat,
+      coordinates.lon
     )
-    weatherData.set(
-      await Weather.getWeather(coordinates.lat, coordinates.lon, FORECAST_DAYS)
+    const weatherRequest = Weather.getWeather(
+      coordinates.lat,
+      coordinates.lon,
+      FORECAST_DAYS
     )
+
+    currentWeatherData.set(await currentWeatherRequest)
+    weatherData.set(await weatherRequest)
   } catch (err) {
     console.error(err)
   }
