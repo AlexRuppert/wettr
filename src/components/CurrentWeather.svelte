4f6c0436e44a4af4311e02930ffc13b566ca52da
AlexRuppert <al>
2024-07-26 18:14:11 +0200

    merge radar button

--- src/components/CurrentWeather.svelte
+++ src/components/CurrentWeather.svelte
@@ -61,13 +61,15 @@
       <div class="w-16 shrink-0 text-text-hard">
         <WeatherIcon className="pt-2 pl-2" icon={weather.icon} />
       </div>
-      <div class="mb-1 flex w-24 shrink-0 justify-center text-5xl font-light">
+      <div
+        class="mb-1 flex w-24 shrink-0 grow justify-center text-5xl font-light"
+      >
         <div class="celsius" class:negative={weather.temperature < 0}>
           {Math.abs(weather.temperature)}
         </div>
       </div>
       <div
-        class="flex h-full w-full justify-between self-center px-2 *:flex *:flex-col *:items-center *:self-center"
+        class="flex h-full w-full justify-end space-x-2 self-center *:flex *:h-full *:w-16 *:flex-col *:items-center *:justify-center"
       >
         <div>
           <SvgIcon className="block size-5" d={humidity} outline />
@@ -91,8 +93,14 @@
             </div>
           </div>
         </div>
-
-        <div>
+        <!-- svelte-ignore a11y_click_events_have_key_events -->
+        <div
+          class="clickable relative"
+          onclick={() => (radarFilmPopupOpened = true)}
+          role="button"
+          tabindex="-1"
+        >
+          <SvgCorner></SvgCorner>
           <WeatherIcon monochrome icon={'rain'} class="size-5" />
           <div class="flex items-center space-x-0.5">
             <div class=" text-right">
@@ -100,29 +108,6 @@
             </div>
             <UnitXperY top="mm" bottom="h" />
           </div>
-        </div>
-      </div>
-    </div>
-    <!-- svelte-ignore a11y_click_events_have_key_events -->
-    <div
-      class="w clickable group flex h-16 w-16 flex-col items-center justify-center"
-      onclick={() => (radarFilmPopupOpened = true)}
-      role="button"
-      tabindex="-1"
-    >
-      <SvgCorner></SvgCorner>
-
-      <div
-        class="grid h-full w-full grid-cols-3 items-center gap-1 p-1 *:-mt-0.5 *:ml-0.5 *:opacity-40"
-      >
-        <WeatherIcon monochrome icon={'rain'} class={'w-5 pt-2'} />
-        <WeatherIcon monochrome icon={'rain'} class={'w-5 pb-2'} />
-        <WeatherIcon monochrome icon={'rain'} class={'w-5 pt-2'} />
-        <div
-          class="col-span-4 pb-1.5 text-center text-sm tracking-wider !opacity-100"
-        >
-          RADA
-          <div class="-ml-1 inline-block -scale-x-100">R</div>
         </div>
       </div>
     </div>
